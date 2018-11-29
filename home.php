5311551f2f8bf84e96e7f37582443c77c6b61e8b
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-29 10:46:14 -0800

    change main page from index to home.php

---
+++ home.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The home page template file
+ *
+ * @package QOD_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<i class="fas fa-quote-left"></i>
+	<div class="quote-div">
+
+		<?php 
+		if ( have_posts() ) :
+			while( have_posts() ): the_post();
+
+			get_template_part( 'template-parts/content' );
+
+			endwhile;
+
+			else:
+				get_template_part( 'template-parts/content', 'none' );
+
+		endif; 
+		?>
+		</div>
+		<i class="fas fa-quote-right"></i>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
