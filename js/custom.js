15b2658fe782a75df983a0036abe89027b2dc4e8
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-29 10:01:13 -0800

    use doc.ready shorthand in js

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
-(function($) {
-    $(document).ready(function() {
+(function ($) {
+    $(function () {
         const $button = $('#new-quote-button');
         const $content = $('.entry-content');
         const $title = $('.entry-title');
         const $source = $('.source');
-        const $submit = $( '#quote-submission-form' );
+        const $submit = $('#quote-submission-form');
         let lastPage = '';
 
         //get a random post and append content to the dom
 
-        $button.on('click', function(event) {
+        $button.on('click', function (event) {
             event.preventDefault();
             //ajaxrequest
-                getQuote();
+            getQuote();
         })//end of ajax function
 
         function getQuote() {
@@ -24,47 +24,48 @@
                 dataType: 'JSON',
             })
 
-            .done(function(data){
-                //figure out the post slug
-                history.pushState( null, null, quotesondev_vars.home_url + '/' + data[0].slug );
+                .done(function (data) {
+                    //figure out the post slug
+                    history.pushState(null, null, quotesondev_vars.home_url + '/' + data[0].slug);
 
-                $content.empty();
-                $content.append(
-                    data[0].content.rendered
+                    $content.empty();
+                    $content.append(
+                        data[0].content.rendered
                     );
-                $title.empty();
-                $title.append(
-                    `&mdash;${data[0].title.rendered}`
+                    $title.empty();
+                    $title.append(
+                        `&mdash;${data[0].title.rendered}`
                     );
                     $source.empty();
-                if(data[0]._qod_quote_source_url.length > 0) {
-                    $source.append(
-                   `, &nbsp; <a href="${data[0]._qod_quote_source_url}">${data[0]._qod_quote_source}</a>`
-                    );
+                    if (data[0]._qod_quote_source_url.length > 0) {
+                        $source.append(
+                            `, &nbsp; <a href="${data[0]._qod_quote_source_url}">${data[0]._qod_quote_source}</a>`
+                        );
 
-                    } else if(data[0]._qod_quote_source > 0){
-                $source.append(
-                   `, &nbsp; ${data[0]._qod_quote_source}`
-                    );} else {
+                    } else if (data[0]._qod_quote_source > 0) {
+                        $source.append(
+                            `, &nbsp; ${data[0]._qod_quote_source}`
+                        );
+                    } else {
                         $source.append('');
                     }
-            })//ajax request .done
+                })//ajax request .done
 
-            .fail(function(){
-                $content.append( 'Sorry, Somethings gone wrong!' );
-            });
+                .fail(function () {
+                    $content.append('Sorry, Somethings gone wrong!');
+                });
         } // end of getQuote
 
-        $(window).on('popstate', function(){
+        $(window).on('popstate', function () {
             window.location.replace(lastPage);
         });
 
 
         //submit the form and create a new quote post 
-        $submit.on( 'submit', function( event ){
+        $submit.on('submit', function (event) {
             event.preventDefault();
             postQuote();
-        } );
+        });
 
         function postQuote() {
             //get values of your form input
@@ -72,7 +73,7 @@
             const $quoteContent = $('#quote-content').val();
             const $quoteSource = $('#quote-source').val();
             const $quoteURL = $('#quote-source-url').val();
-            
+
             $.ajax({
                 url: quotesondev_vars.rest_url + 'wp/v2/posts',
                 method: 'POST',
@@ -83,21 +84,21 @@
                     _qod_quote_source_url: $quoteURL,
                     status: 'publish',
                 },
-                beforeSend: function(xhr){
-                    xhr.setRequestHeader( 'X-WP-NONCE', quotesondev_vars.nonce )
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('X-WP-NONCE', quotesondev_vars.nonce)
                 }
 
             })
 
-            .done( function(){
-                $submit.slideUp(1000);
-                $submit.parent().append( '<h3>Thanks, your submission was recieved!</h3>' );
-            } )
+                .done(function () {
+                    $submit.slideUp(1000);
+                    $submit.parent().append('<h3>Thanks, your submission was recieved!</h3>');
+                })
 
-            .fail( function(){
-                $submit.empty();
-                $submit.append( '<h3>Sorry, please try again later.</h3>' );
-            } );
+                .fail(function () {
+                    $submit.empty();
+                    $submit.append('<h3>Sorry, please try again later.</h3>');
+                });
         }//end of post quote
     });//end of doc ready
 })(jQuery)
