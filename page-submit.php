58175b2e0c887d2939ff1076e66842c7427af90e
Jenna Pegg <jenna.pegg.vic@gmail.com>
2018-11-28 16:21:34 -0800

    fix submit quote link, adjust desktop size, rename categories and tags partial

--- page-submit.php
+++ page-submit.php
@@ -52,7 +52,7 @@ get_header(); ?>
             else:
             ?>
             <p>Sorry, you must be logged in to submit a quote!</p>
-            <p><?php echo sprintf('<a href"%1s">%2s</a>', esc_url( wp_login_url() ), 'Click here to login') ?></p>
+            <p><?php echo sprintf('<a href="%1s">%2s</a>', esc_url( wp_login_url() ), 'Click here to login') ?></p>
             <?php
             endif;
             ?>
